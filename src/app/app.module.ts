cf4f1b2bf6c2150528c4e0d91797b1e94cf99ecf
Carlos Luis Bautista De Jesús <cdejesusdx@hotmail.com>
2021-03-08 19:52:41 -0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { LaunchNavigator } from '@ionic-native/launch-navigator/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -11,7 +13,12 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: 
+  [
+    Geolocation,
+    LaunchNavigator,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavController} from '@ionic/angular';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  // Datos del usuario:
 
+  latitude:number; // latitud 
+  longitude:number; // longitud
+
+  constructor(public navController: NavController, public geolocation: Geolocation, public launchNavigator:LaunchNavigator) {
+    this.getGeolocation();
+  }
+  
+  // Obtener las coordenadas del usuario
+  getGeolocation(){
+    this.geolocation.getCurrentPosition().then(position =>{
+          this.latitude = position.coords.latitude;
+          this.longitude = position.coords.longitude;
+      },error=>{
+          console.log('error',error);
+      });
+  }
+
+  navigateLocation(){
+    let options: LaunchNavigatorOptions = {
+      start:[this.latitude,this.longitude],
+      app: this.launchNavigator.APP.GOOGLE_MAPS
+    };
+
+    this.launchNavigator.navigate('London, ON', options).then(success =>{
+      console.log(success);
+    },error=>{
+      console.log(error);
+    })
+  }
 }
